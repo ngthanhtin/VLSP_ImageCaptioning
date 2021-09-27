ae3f75abb7a8204d9c52a8219be1b0d1222e3754
nguyen_thanh_tin <ngthanhtinqn@gmail.com>
2021-09-27 10:33:46 +0900

    change to batched beam search

--- inference.py
+++ inference.py
@@ -61,7 +61,43 @@ seed_torch(seed = CFG.seed)
 tokenizer = torch.load('./tokenizers/tokenizer_vi_not_remove_single_character.pth')
 # print(f"tokenizer.stoi: {tokenizer.stoi}")
 
-def inference_with_beam_search(test_loader, encoder, decoder, tokenizer, device, beam_size=3):
+# def beam_search_predictions(image, beam_index = 3):
+#     start = [wordtoix["startseq"]]
+#     start_word = [[start, 0.0]]
+#     while len(start_word[0][0]) < max_length:
+#         temp = []
+#         for s in start_word:
+#             par_caps = sequence.pad_sequences([s[0]], maxlen=max_length, padding='post')
+#             preds = model.predict([image,par_caps], verbose=0)
+#             word_preds = np.argsort(preds[0])[-beam_index:]
+#             # Getting the top <beam_index>(n) predictions and creating a 
+#             # new list so as to put them via the model again
+#             for w in word_preds:
+#                 next_cap, prob = s[0][:], s[1]
+#                 next_cap.append(w)
+#                 prob += preds[0][w]
+#                 temp.append([next_cap, prob])
+                    
+#         start_word = temp
+#         # Sorting according to the probabilities
+#         start_word = sorted(start_word, reverse=False, key=lambda l: l[1])
+#         # Getting the top words
+#         start_word = start_word[-beam_index:]
+    
+#     start_word = start_word[-1][0]
+#     intermediate_caption = [ixtoword[i] for i in start_word]
+#     final_caption = []
+    
+#     for i in intermediate_caption:
+#         if i != 'endseq':
+#             final_caption.append(i)
+#         else:
+#             break
+    
+#     final_caption = ' '.join(final_caption[1:])
+#     return final_caption
+
+def inference_with_batched_beam_search(test_loader, encoder, decoder, tokenizer, device, beam_size=3):
 
     encoder.eval()
     decoder.eval()
@@ -295,7 +331,7 @@ if CFG.ensemble == False:
     gc.collect()
 
     # Inference
-    predictions  = inference_with_beam_search(test_loader, encoder, decoder, tokenizer, device)
+    predictions  = inference_with_batched_beam_search(test_loader, encoder, decoder, tokenizer, device)
 else:
     print("Predicting with Ensemble.....")
     model1 = './pretrained_models/swin_fold0_best_bleu.pth'
