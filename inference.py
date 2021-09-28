e505f2181c3d1eb9a2296c678bb71a4af8d9c51d
nguyen_thanh_tin <ngthanhtinqn@gmail.com>
2021-09-28 14:48:18 +0900

    delete logger inference

--- inference.py
+++ inference.py
@@ -33,21 +33,6 @@ from models.model import CNN, DecoderWithAttention
 
 device = CFG.device
 
-def init_logger(log_file=OUTPUT_DIR+'train.log'):
-    from logging import getLogger, INFO, FileHandler,  Formatter,  StreamHandler
-    logger = getLogger(__name__)
-    logger.setLevel(INFO)
-    handler1 = StreamHandler()
-    handler1.setFormatter(Formatter("%(message)s"))
-    handler2 = FileHandler(filename=log_file)
-    handler2.setFormatter(Formatter("%(message)s"))
-    logger.addHandler(handler1)
-    logger.addHandler(handler2)
-    return logger
-
-LOGGER = init_logger()
-
-
 def seed_torch(seed=42):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
