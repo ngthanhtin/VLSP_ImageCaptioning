41fd200c0a45fcfeb9d78b5edfb93b38460d930a
nguyen_thanh_tin <ngthanhtinqn@gmail.com>
2021-10-01 11:25:44 +0900

    remove denoise

--- dataset.py
+++ dataset.py
@@ -23,27 +23,10 @@ class TrainDataset(Dataset):
     def __len__(self):
         return len(self.df)
     
-    def denoise(self, image):
-        b = 2
-        kernel = (1 / (b + 2) ** 2) * np.array([[1, b, 1], [b, b ** 2, b], [1, b, 1]])
-        dst = cv2.filter2D(image, -1, kernel)
-        dst_denoise = cv2.fastNlMeansDenoisingColored(dst, None, 10, 10, 7, 21)
-#         plt.hist(dst.ravel())
-#         plt.show()
-        gray = cv2.cvtColor(dst_denoise, cv2.COLOR_BGR2GRAY)
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
-        cl1 = clahe.apply(gray)
-        cl1[cl1 >= 240] = 255
-        output = cv2.cvtColor(cl1, cv2.COLOR_GRAY2BGR)
-        kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-        output = cv2.filter2D(output, -1, kernel)
-        return output
-    
     def __getitem__(self, idx):
         file_path = self.file_paths[idx]
         image = cv2.imread(file_path)
-        
-#         image = self.denoise(image)
+
         try:
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
         except:
